4ebe2fef373e63488ad8bad7bc7938b8ee01b814
hex <sasha.nef@yandex.ru>
2023-12-26 20:24:35 +0300

    Добавляет функции из задания

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // Проверка на палиндром
 const isPalindrome = (word) => {
   const tempWord = word.toLowerCase().replaceAll(' ', '');
@@ -7,7 +8,12 @@ const isPalindrome = (word) => {
   }
 
   return tempWord === palindrome;
-}
+};
+
+console.log(isPalindrome('топот')); // true
+console.log(isPalindrome('ДовОд')); // true
+console.log(isPalindrome('Кекс')); // false
+console.log(isPalindrome('Лёша на полке клопа нашёл ')); // true
 
 // Извлекает числа из строки
 const getNumbers = (string) => {
@@ -27,7 +33,17 @@ const getNumbers = (string) => {
     case false:
       return +number;
   }
-}
+};
+
+console.log(getNumbers('2023 год')); // 2023
+console.log(getNumbers('ECMAScript 2022')); // 2022
+console.log(getNumbers('1 кефир, 0.5 батона')); // 105
+console.log(getNumbers('агент 007')); // 7
+console.log(getNumbers('а я томат')); // NaN
+console.log(getNumbers(2023)); // 2023
+console.log(getNumbers(-1)); // 1
+console.log(getNumbers(1.5)); // 15
+
 
 // Добавляет символы в начале строки
 const padString = (string, minLength, pad) => {
@@ -38,8 +54,17 @@ const padString = (string, minLength, pad) => {
   }
 
   return pad.slice(0, compensation % pad.length) + pad.repeat(compensation / pad.length) + string;
-}
+};
+
+console.log(padString('1', 2, '0')); // '01'
+console.log(padString('1', 4, '0')); // '0001'
+console.log(padString('q', 4, 'werty')); // 'werq'
+console.log(padString('q', 4, 'we')); // 'wweq'
+console.log(padString('qwerty', 4, '0')); // 'qwerty'
 
 // Проверяет длину строки
 const checkStringLength = (string, length) => (string.length <= length);
 
+console.log(checkStringLength('проверяемая строка', 20)); // true
+console.log(checkStringLength('проверяемая строка', 18)); // true
+console.log(checkStringLength('проверяемая строка', 10)); // false
