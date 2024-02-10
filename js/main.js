50853eb7bc0f866db3e78112e0140a58ba1d88ff
hex <sasha.nef@yandex.ru>
2024-02-10 14:39:53 +0300

    испÐрÐавления

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-import './rendersPosts.js';
+import {renderPosts} from './rendersPosts.js';
+import {PICTURE_COUNT} from './data.js';
+import {createPhotoDescriptions} from './createPhotoDescriptions.js';
+
+renderPosts(createPhotoDescriptions(PICTURE_COUNT));
