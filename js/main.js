2ae11a2bad635dc8517dcdce88b36db2f6bcd5fa
hex <sasha.nef@yandex.ru>
2024-03-13 15:03:35 +0300

    WIP

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import {renderPosts} from './render-posts.js';
 import {dataArray} from './create-photo-descriptions.js';
 import {initModalForm} from './modal-form.js';
+import {getData} from './get-data.js';
 
 renderPosts(dataArray);
+console.log(dataArray);
+getData();
 initModalForm();
