92fc33d0207a35fd8bb535bf2663b162f48bfa3b
hex <sasha.nef@yandex.ru>
2024-03-15 10:10:14 +0300

    WIP

--- js/send-data.js
+++ js/send-data.js
@@ -4,49 +4,94 @@ const templateMessageSuccessModal = document
   .querySelector('#success')
   .content
   .querySelector('.success');
+const messageSuccessModal = templateMessageSuccessModal.cloneNode(true);
+const buttonCloseSuccess = messageSuccessModal.querySelector('.success__button');
 
 const templateMessageErrorModal = document
   .querySelector('#error')
   .content
   .querySelector('.error');
+const messageErrorModal = templateMessageErrorModal.cloneNode(true);
+const buttonCloseError = messageErrorModal.querySelector('.error__button');
 
-
-const showMessageModal = (template) => {
-  const messageModal = template.cloneNode(true);
-  const buttonClose = messageModal.querySelector('button');
-
-  const closeModal = () => {
-    document.body.removeChild(messageModal);
-    removeEventListeners();
-  };
-
-
-  const onCloseModal = (evt) => {
-    if (evt.target === messageModal || evt.target === buttonClose) {
-      closeModal();
-    }
-  };
-
-  const onEscCloseModal = (evt) => {
-    if (isEscapeKey(evt)) {
-      closeModal();
-    }
-  };
-  const removeEventListeners = () => {
-    messageModal.removeEventListener('click', onCloseModal);
-    document.body.removeEventListener('keydown', onEscCloseModal);
-  };
-
-  messageModal.addEventListener('click', onCloseModal);
-  document.body.addEventListener('keydown', onEscCloseModal);
-
-  document.body.appendChild(messageModal);
+const onCloseSuccess = (evt) => {
+  if (evt.target === messageSuccessModal || evt.target === buttonCloseSuccess) {
+    messageSuccessModal.removeEventListener('click', onCloseSuccess);
+    document.body.removeChild(messageSuccessModal);
+  }
 };
+
+const onEscCloseSuccess = (evt) => {
+  if (isEscapeKey(evt)) {
+    document.body.removeEventListener('keydown', onEscCloseSuccess);
+    document.body.removeChild(messageSuccessModal);
+  }
+};
+
+const showMessageSuccess = () => {
+  messageSuccessModal.addEventListener('click', onCloseSuccess);
+  document.body.addEventListener('keydown', onEscCloseSuccess);
+
+  document.body.appendChild(messageSuccessModal);
+};
+
+const onCloseError = (evt) => {
+  if (evt.target === messageErrorModal || evt.target === buttonCloseError) {
+    messageErrorModal.removeEventListener('click', onCloseError);
+    document.body.removeChild(messageErrorModal);
+  }
+};
+
+const onEscCloseError = (evt) => {
+  if (isEscapeKey(evt)) {
+    document.body.removeEventListener('keydown', onEscCloseError);
+    document.body.removeChild(messageErrorModal);
+  }
+};
+
+const showMessageError = () => {
+  messageErrorModal.addEventListener('click', onCloseError);
+  document.body.addEventListener('keydown', onEscCloseError);
+
+  document.body.appendChild(messageErrorModal);
+};
+
+// const showMessageModal = (template) => {
+//   const messageModal = template.cloneNode(true);
+//   const buttonClose = messageModal.querySelector('button');
+//
+//   const closeModal = () => {
+//     document.body.removeChild(messageModal);
+//     removeEventListeners();
+//   };
+//
+//
+//   const onCloseModal = (evt) => {
+//     if (evt.target === messageModal || evt.target === buttonClose) {
+//       closeModal();
+//     }
+//   };
+//
+//   const onEscCloseModal = (evt) => {
+//     if (isEscapeKey(evt)) {
+//       closeModal();
+//     }
+//   };
+//   const removeEventListeners = () => {
+//     messageModal.removeEventListener('click', onCloseModal);
+//     document.body.removeEventListener('keydown', onEscCloseModal);
+//   };
+//
+//   messageModal.addEventListener('click', onCloseModal);
+//   document.body.addEventListener('keydown', onEscCloseModal);
+//
+//   document.body.appendChild(messageModal);
+// };
 
 export const sendData = (onSuccess, formData) => {
 
   fetch(
-    'https://28.javascript.htmlacademy.pro/kekstagram',
+    'https://28.javascript.htmlacademy.pro/kekstagra',
     {
       method: 'POST',
       body: formData,
@@ -57,8 +102,12 @@ export const sendData = (onSuccess, formData) => {
         throw new Error();
       } else {
         onSuccess();
-        showMessageModal(templateMessageSuccessModal);
+        showMessageSuccess();
+        // showMessageModal(templateMessageSuccessModal);
       }
     })
-    .catch(() => showMessageModal(templateMessageErrorModal));
+    .catch(() => {
+      showMessageError();
+      // showMessageModal(templateMessageErrorModal)
+    });
 };
